8a8afd94d356a71802b72739abb8aae2e721f50e
LeviN09 <nemnagylevi@gmail.com>
2024-05-12 20:01:39 +0200

    three tape test

--- testing/turing_machine.test.js
+++ testing/turing_machine.test.js
@@ -149,5 +149,142 @@ describe('Turing Machine Tests', () => {
             expect(tm.isOverLimit()).toBe(false);
             expect(tm.tapes[0]).toStrictEqual(["0", "1", "0", "1", "0", "1", "_"]);
         });
+        test('Shift three-way xor', () => {
+            const tm = new TuringMachine(['q0', 'q1', 'q2'],
+                ['0', '1'],
+                {
+                    q0: {
+                        '_,_,_': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '1', moveDirection: 'R'},
+                                { writeSymbol: '_', moveDirection: 'S'},
+                                { writeSymbol: '_', moveDirection: 'S'}
+                            ]
+                        },
+                        '0,_,_': {
+                            nextState: 'q0',
+                            transition: [
+                                { writeSymbol: '0', moveDirection: 'R'},
+                                { writeSymbol: '1', moveDirection: 'R'},
+                                { writeSymbol: '0', moveDirection: 'R'}
+                            ]
+                        },
+                        '1,_,_': {
+                            nextState: 'q0',
+                            transition: [
+                                { writeSymbol: '1', moveDirection: 'R'},
+                                { writeSymbol: '0', moveDirection: 'R'},
+                                { writeSymbol: '1', moveDirection: 'R'}
+                            ]
+                        }
+                    },
+                    q1: {
+                        '_,_,_': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        },
+                        '0,_,_': {
+                            nextState: 'q2',
+                            transition: [
+                                { writeSymbol: '0', moveDirection: 'S'},
+                                { writeSymbol: '_', moveDirection: 'S'},
+                                { writeSymbol: '_', moveDirection: 'S'}
+                            ]
+                        },
+                        '1,_,_': {
+                            nextState: 'q2',
+                            transition: [
+                                { writeSymbol: '1', moveDirection: 'S'},
+                                { writeSymbol: '_', moveDirection: 'S'},
+                                { writeSymbol: '_', moveDirection: 'S'}
+                            ]
+                        },
+                        '0,0,0': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '0', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        },
+                        '1,0,0': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '1', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        },
+                        '0,1,0': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '1', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        },
+                        '0,0,1': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '1', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        },
+                        '0,1,1': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '0', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        },
+                        '1,0,1': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '0', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        },
+                        '1,1,0': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '0', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        },
+                        '1,1,1': {
+                            nextState: 'q1',
+                            transition: [
+                                { writeSymbol: '1', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'},
+                                { writeSymbol: '_', moveDirection: 'L'}
+                            ]
+                        }
+                    }
+                },
+                'q0',
+                ['q2'],
+                3,
+                [0,0,0]
+            );
+            tm.tapes[0] = ['1', '0', '1', '0', '1'];
+            tm.tapes[1] = ['_'];
+            tm.tapes[2] = ['_'];
+
+            while (tm.step()) {}
+
+            expect(tm.currentState).toBe('q2');
+            expect(tm.isHalted()).toBe(true);
+            expect(tm.isOverLimit()).toBe(false);
+            expect(tm.tapes[0]).toStrictEqual(["1", "1", "0", "1", "0", "0", "_"]);
+        });
     });
 });
